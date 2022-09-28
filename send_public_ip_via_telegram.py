4370b393bfedce403fb35620f2e502ea5c547422
KNosir <94743032+KNosir@users.noreply.github.com>
2022-09-28 08:17:26 +0500

    Update send_public_ip_via_telegram.py

--- send_public_ip_via_telegram.py
+++ send_public_ip_via_telegram.py
@@ -1,15 +1,9 @@
-from requests import post
-from socket import gethostname, gethostbyname
-from requests import get
+from requests import post, get
 from time import sleep
 
 ip = get('https://api.ipify.org').text
 
 time_in_second = 1000
-
-
-host_name = gethostname()
-IP_adderss = gethostbyname(host_name)
 
 def send_to_telegram(message):
 
@@ -18,11 +12,11 @@ def send_to_telegram(message):
     apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
 
     try:
-        response_Sadam = post(apiURL, json={'chat_id': chatID, 'text': message})
+        response = post(apiURL, json={'chat_id': chatID, 'text': message})
         print(response.text)
     except Exception as e:
         print(e)
-    print(type(response_Nosir.text))
+    print(type(response.text))
 while True:
     send_to_telegram(ip)
     sleep(time_in_second)
